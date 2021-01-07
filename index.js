6e73f2160ac1fde916db22f2794be10a69fcf72f
jyand <jyand@protonmail.com>
2021-01-07 15:51:28 +0000

    nice

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-// HTML elements whose sub-elements are changed
 var m = document.getElementById("main");
-//t = document.getElementById("top").innerHTML ;
 var ins = document.getElementById("insert");
 var cre = document.getElementById("create");
-// textbox for the table name
 function InsertInto(e) {
-    m.innerHTML = '<form id="table"><label for="tableName">Table Name:<input type="text" id="tableName" name="tableName"></form></br>';
-    m.innerHTML += '<form id="fields"><label for="field0">Fields:<input type="text" id="field0" name="field0"></form></br>';
+    m.innerHTML = '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>';
+    m.innerHTML += '<form id="field"><label for="field0"><p>Fields:</p><input type="text" id="field0" name="field0"></form></br></br>';
+    m.innerHTML += '<form id="fields"><label for="numFields">Number of Fields:<input type="number" id="numFields" name="numFields"></form></br>';
+    m.innerHTML += '<form id="value"><label for="value0"><p>Values:</p><input type="text" id="value0" name="value"></form></br>';
+    m.innerHTML += '<form id="values"><label for="value0">Number of Values<input type="number" id="value0" name="value"></form></br></br>';
     document.getElementById("table").addEventListener("keyup", function () {
-        var str = document.forms["table"]["tableName"].value + " (";
-        document.getElementById("top").innerHTML = "INSERT INTO " + str;
+        var str = document.forms["table"]["tableName"].value;
+        document.getElementById("tbl").textContent = "INSERT INTO " + str;
     });
-    document.getElementById("fields").addEventListener("keyup", function () {
-        var f = document.forms["fields"];
-        var s = "";
-        for (var i; i < f.length; ++i) {
-            s += f[i] + ", ";
-        }
-        document.getElementById("top").innerHTML += s;
+    document.getElementById("field").addEventListener("keyup", function () {
+        var f = document.forms["field"];
+        var str = f["field0"].value;
+        /*for (var i: number ; i < f.length ; ++i) {
+                s += f[i].value + ", " ;
+        }*/
+        document.getElementById("fld").textContent = "(" + str + ")" + " VALUES";
+    });
+    document.getElementById("value").addEventListener("keyup", function () {
+        var v = document.forms["value"];
+        var str = v["value0"].value;
+        /*for (var i: number ; i < v.length ; ++i) {
+                s += v[i].value + ", " ;
+        }*/
+        document.getElementById("val").textContent = "(" + str + ")" + ";";
     });
 }
 // textboxes for inserting data into existing tables
--- index.ts
+++ index.ts
@@ -1,25 +1,36 @@
-// HTML elements whose sub-elements are changed
 var m = document.getElementById("main") ;
-//t = document.getElementById("top").innerHTML ;
 var ins = document.getElementById("insert") ;
 var cre = document.getElementById("create") ;
 
-// textbox for the table name
 function InsertInto(e): void {
-        m.innerHTML = '<form id="table"><label for="tableName">Table Name:<input type="text" id="tableName" name="tableName"></form></br>' ;
-        m.innerHTML += '<form id="fields"><label for="field0">Fields:<input type="text" id="field0" name="field0"></form></br>' ;
+        m.innerHTML = '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>' ;
+        m.innerHTML += '<form id="field"><label for="field0"><p>Fields:</p><input type="text" id="field0" name="field0"></form></br></br>' ;
+        m.innerHTML += '<form id="fields"><label for="numFields">Number of Fields:<input type="number" id="numFields" name="numFields"></form></br>' ;
+        m.innerHTML += '<form id="value"><label for="value0"><p>Values:</p><input type="text" id="value0" name="value"></form></br>' ;
+        m.innerHTML += '<form id="values"><label for="value0">Number of Values<input type="number" id="value0" name="value"></form></br></br>' ;
         document.getElementById("table").addEventListener("keyup", function() {
-                let str = document.forms["table"]["tableName"].value + " (" ;
-                document.getElementById("top").innerHTML = "INSERT INTO " + str ;
+                let str: string = document.forms["table"]["tableName"].value ;
+                document.getElementById("tbl").textContent = "INSERT INTO " + str ;
         }) ;
-        document.getElementById("fields").addEventListener("keyup", function() {
-                let f = document.forms["fields"] ;
-                let s = "" ;
-                for (var i: number ; i < f.length ; ++i) {
-                        s += f[i] + ", " ;
-                }
-                document.getElementById("top").innerHTML += s ;
+        document.getElementById("field").addEventListener("keyup", function() {
+                let f = document.forms["field"] ;
+                let str: string = f["field0"].value ;
+                /*for (var i: number ; i < f.length ; ++i) {
+                        s += f[i].value + ", " ;
+                }*/
+                document.getElementById("fld").textContent = "(" + str + ")" + " VALUES" ;
         }) ;
+        document.getElementById("value").addEventListener("keyup", function() {
+                let v = document.forms["value"] ;
+                let str: string = v["value0"].value ;
+                /*for (var i: number ; i < v.length ; ++i) {
+                        s += v[i].value + ", " ;
+                }*/
+                document.getElementById("val").textContent = "(" + str + ")" + ";" ;
+        }) ;
+}
+
+function IncrementInput(el): void {
 }
 
 // textboxes for inserting data into existing tables
