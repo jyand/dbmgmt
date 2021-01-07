5aae62295b83b92705e3e3180bb173ea0dcc9844
jyand <jyand@protonmail.com>
2021-01-07 19:35:41 +0000

    UI for INSERT module completely functional

--- insert.ts
+++ insert.ts
@@ -1,24 +1,55 @@
 var m = document.getElementById("main") ;
 var ins = document.getElementById("insert") ;
-var cre = document.getElementById("create") ;
 
-function InsertInto(e): void {
+function FixInput(id: string): void {
+        let n: number = document.forms[`${id}s`][`num${id}s`].value ;
+        if (n < 1) {
+                n = 1 ;
+                document.forms[`${id}s`][`num${id}s`].value = n ;
+        }
+}
+
+function IncrementInput(id: string): void {
+        document.getElementById(id).innerHTML = "" ;
+        let n: number = document.forms[`${id}s`][`num${id}s`].value ;
+        for (let i: number = 0 ; i < n ; ++i) {
+                document.getElementById(id).innerHTML += `<label for="${id}${i}"><input type="text" id="${id}${i}" name="${id}${i}">` ;
+        }
+}
+
+function StringFromInput(id: string): string {
+        let f = document.forms[id] ;
+        let N: number = f.length ;
+        let str: string = "(" ;
+        for (let i: number = 0 ; i < N ; ++i) {
+                str += f[`${id}${i}`].value ;
+                if (i === N - 1) {
+                        str += ")" ;
+                } else {
+                        str += ", " ;
+                }
+        }
+        if (id === "field") {
+                str += " VALUES" ;
+        }
+        if (id === "value") {
+                str += " ;" ;
+        }
+        return str ;
+}
+
+function Main(e): void {
         m.innerHTML = '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>' ;
         m.innerHTML += '<p>Fields:</p><form id="field"><label for="field0"><input type="text" id="field0" name="field0"></form></br></br>' ;
-        m.innerHTML += '<form id="fields"><label for="numfields">Number of Fields:<input type="number" id="numfields" name="numfields" value="1"></form></br>' ;
+        m.innerHTML += '<form id="fields"><label for="numfields"><input type="number" id="numfields" name="numfields" value="1"></form></br>' ;
         m.innerHTML += '<p>Values:</p><form id="value"><label for="value0"><input type="text" id="value0" name="value0"></form></br></br>' ;
-        m.innerHTML += '<form id="values"><label for="numvalues">Number of Values:<input type="number" id="numvalues" name="numvalues" value="1"></form></br>' ;
+        m.innerHTML += '<form id="values"><label for="numvalues"><input type="number" id="numvalues" name="numvalues" value="1"></form></br>' ;
         document.getElementById("table").addEventListener("keyup", function(): void {
                 let str: string = document.forms["table"]["tableName"].value ;
                 document.getElementById("tbl").textContent = "INSERT INTO " + str ;
         }) ;
         document.getElementById("field").addEventListener("keyup", function(): void {
-                let f = document.forms["field"] ;
-                let str: string = f["field0"].value ;
-                /*for (var i: number ; i < f.length ; ++i) {
-                        s += f[i].value + ", " ;
-                }*/
-                document.getElementById("fld").textContent = "(" + str + ")" + " VALUES" ;
+                document.getElementById("fld").textContent = StringFromInput("field") ;
         }) ;
         document.getElementById("fields").addEventListener("keyup", function(): void {
                 FixInput("field") ;
@@ -33,12 +64,7 @@ function InsertInto(e): void {
                 IncrementInput("field") ;
         }) ;
         document.getElementById("value").addEventListener("keyup", function(): void {
-                let v = document.forms["value"] ;
-                let str: string = v["value0"].value ;
-                /*for (var i: number ; i < v.length ; ++i) {
-                        s += v[i].value + ", " ;
-                }*/
-                document.getElementById("val").textContent = "(" + str + ")" + ";" ;
+                document.getElementById("val").textContent = StringFromInput("value") ;
         }) ;
         document.getElementById("values").addEventListener("keyup", function(): void {
                 FixInput("value") ;
@@ -54,24 +80,4 @@ function InsertInto(e): void {
         }) ;
 }
 
-function FixInput(id: string): void {
-        let n: number = document.forms[`${id}s`][`num${id}s`].value ;
-        if (n < 1) {
-                n = 1 ;
-                document.forms[`${id}s`][`num${id}s`].value = n ;
-        }
-}
-
-function IncrementInput(id: string): void {
-        document.getElementById(id).innerHTML = "" ;
-        let n: number = document.forms[`${id}s`][`num${id}s`].value ;
-        for (var i: number = 0 ; i < n ; ++i) {
-                document.getElementById(id).innerHTML += `<label for="field${i}"><input type="text" id="field${i}" name="field${i}">` ;
-        }
-}
-
-// textboxes for inserting data into existing tables
-/*function addFields(size: number): void {
-}*/
-
-ins.addEventListener("click", InsertInto) ;
+ins.addEventListener("click", Main) ;
--- scratch.ts
+++
@@ -1,6 +0,0 @@
-var str: string = 'DJ Khaled: ' ;
-for (var i: number = 1 ; i < 10 ; ++i) {
-        str += ` and another ${i}` ;
-}
-console.log(str) ;
-console.log(typeof(str)) ;
