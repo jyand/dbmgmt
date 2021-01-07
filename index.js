c31ea65138cf8890b3d195445375d06984e3c13d
jyand <jyand@protonmail.com>
2021-01-07 17:56:28 +0000

    incrementing/decrementing input works well

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var ins = document.getElementById("insert");
 var cre = document.getElementById("create");
 function InsertInto(e) {
     m.innerHTML = '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>';
-    m.innerHTML += '<form id="field"><label for="field0"><p>Fields:</p><input type="text" id="field0" name="field0"></form></br></br>';
+    m.innerHTML += '<p>Fields:</p><form id="field"><label for="field0"><input type="text" id="field0" name="field0"></form></br></br>';
     m.innerHTML += '<form id="fields"><label for="numfields">Number of Fields:<input type="number" id="numfields" name="numfields" value="1"></form></br>';
-    m.innerHTML += '<form id="value"><label for="value0"><p>Values:</p><input type="text" id="value0" name="value0"></form></br></br>';
-    m.innerHTML += '<form id="values"><label for="numvalues">Number of Values<input type="number" id="numvalues" name="numvalues" value="1"></form></br>';
+    m.innerHTML += '<p>Values:</p><form id="value"><label for="value0"><input type="text" id="value0" name="value0"></form></br></br>';
+    m.innerHTML += '<form id="values"><label for="numvalues">Number of Values:<input type="number" id="numvalues" name="numvalues" value="1"></form></br>';
     document.getElementById("table").addEventListener("keyup", function () {
         var str = document.forms["table"]["tableName"].value;
         document.getElementById("tbl").textContent = "INSERT INTO " + str;
@@ -25,6 +25,12 @@ function InsertInto(e) {
     document.getElementById("fields").addEventListener("click", function () {
         FixInput("field");
     });
+    document.getElementById("fields").addEventListener("keyup", function () {
+        IncrementInput("field");
+    });
+    document.getElementById("fields").addEventListener("click", function () {
+        IncrementInput("field");
+    });
     document.getElementById("value").addEventListener("keyup", function () {
         var v = document.forms["value"];
         var str = v["value0"].value;
@@ -39,6 +45,12 @@ function InsertInto(e) {
     document.getElementById("values").addEventListener("click", function () {
         FixInput("value");
     });
+    document.getElementById("values").addEventListener("keyup", function () {
+        IncrementInput("value");
+    });
+    document.getElementById("values").addEventListener("click", function () {
+        IncrementInput("value");
+    });
 }
 function FixInput(id) {
     var n = document.forms[id + "s"]["num" + id + "s"].value;
@@ -47,11 +59,13 @@ function FixInput(id) {
         document.forms[id + "s"]["num" + id + "s"].value = n;
     }
 }
-/*function IncrementInput(id: string): void {
-        let n: number = document.forms[`${id}s`][`num${id}s`].value ;
-        if (n > 1) {
-        }
-}*/
+function IncrementInput(id) {
+    document.getElementById(id).innerHTML = "";
+    var n = document.forms[id + "s"]["num" + id + "s"].value;
+    for (var i = 0; i < n; ++i) {
+        document.getElementById(id).innerHTML += "<label for=\"field" + i + "\"><input type=\"text\" id=\"field" + i + "\" name=\"field" + i + "\">";
+    }
+}
 // textboxes for inserting data into existing tables
 /*function addFields(size: number): void {
 }*/
--- index.ts
+++ index.ts
@@ -4,10 +4,10 @@ var cre = document.getElementById("create") ;
 
 function InsertInto(e): void {
         m.innerHTML = '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>' ;
-        m.innerHTML += '<form id="field"><label for="field0"><p>Fields:</p><input type="text" id="field0" name="field0"></form></br></br>' ;
+        m.innerHTML += '<p>Fields:</p><form id="field"><label for="field0"><input type="text" id="field0" name="field0"></form></br></br>' ;
         m.innerHTML += '<form id="fields"><label for="numfields">Number of Fields:<input type="number" id="numfields" name="numfields" value="1"></form></br>' ;
-        m.innerHTML += '<form id="value"><label for="value0"><p>Values:</p><input type="text" id="value0" name="value0"></form></br></br>' ;
-        m.innerHTML += '<form id="values"><label for="numvalues">Number of Values<input type="number" id="numvalues" name="numvalues" value="1"></form></br>' ;
+        m.innerHTML += '<p>Values:</p><form id="value"><label for="value0"><input type="text" id="value0" name="value0"></form></br></br>' ;
+        m.innerHTML += '<form id="values"><label for="numvalues">Number of Values:<input type="number" id="numvalues" name="numvalues" value="1"></form></br>' ;
         document.getElementById("table").addEventListener("keyup", function(): void {
                 let str: string = document.forms["table"]["tableName"].value ;
                 document.getElementById("tbl").textContent = "INSERT INTO " + str ;
@@ -26,6 +26,12 @@ function InsertInto(e): void {
         document.getElementById("fields").addEventListener("click", function(): void {
                 FixInput("field") ;
         }) ;
+        document.getElementById("fields").addEventListener("keyup", function(): void {
+                IncrementInput("field") ;
+        }) ;
+        document.getElementById("fields").addEventListener("click", function(): void {
+                IncrementInput("field") ;
+        }) ;
         document.getElementById("value").addEventListener("keyup", function(): void {
                 let v = document.forms["value"] ;
                 let str: string = v["value0"].value ;
@@ -40,6 +46,12 @@ function InsertInto(e): void {
         document.getElementById("values").addEventListener("click", function(): void {
                 FixInput("value") ;
         }) ;
+        document.getElementById("values").addEventListener("keyup", function(): void {
+                IncrementInput("value") ;
+        }) ;
+        document.getElementById("values").addEventListener("click", function(): void {
+                IncrementInput("value") ;
+        }) ;
 }
 
 function FixInput(id: string): void {
@@ -50,11 +62,13 @@ function FixInput(id: string): void {
         }
 }
 
-/*function IncrementInput(id: string): void {
+function IncrementInput(id: string): void {
+        document.getElementById(id).innerHTML = "" ;
         let n: number = document.forms[`${id}s`][`num${id}s`].value ;
-        if (n > 1) {
+        for (var i: number = 0 ; i < n ; ++i) {
+                document.getElementById(id).innerHTML += `<label for="field${i}"><input type="text" id="field${i}" name="field${i}">` ;
         }
-}*/
+}
 
 // textboxes for inserting data into existing tables
 /*function addFields(size: number): void {
