39698197ae41c0e705e24c575ec59ea22e2fd625
jyand <jyand@protonmail.com>
2021-01-09 17:46:39 +0000

    might limit to SELECT * for simplicity

--- select.js
+++ select.js
@@ -11,10 +11,38 @@ function StringFromFields() {
     }
     return s;
 }
+function OperatorOptions() {
+    var ops = ["(search)", "=", "!=", "<", ">", "<=", ">="];
+    var s = '<select id="ops">';
+    for (var i = 0; i < ops.length; ++i) {
+        s += "<option value=\"" + ops[i] + "\">" + ops[i] + "</option>";
+    }
+    s += "</select>";
+    return s;
+}
+function StringFromCriteria() {
+    var crt = document.forms["criteria"];
+    var op = crt["ops"].value;
+    var cnd = crt["condition0"].value;
+    var s = crt["criteria0"].value;
+    if (op == "(search)") {
+        s += " LIKE '_" + cnd + "_' ";
+    }
+    else {
+        s += " " + op + " ";
+        if (isNaN(cnd)) {
+            s += "'" + cnd + "'";
+        }
+        else {
+            s += "" + cnd;
+        }
+    }
+    return s;
+}
 function Main(e) {
     m.innerHTML = '<p>Fields:</p><form id="field"><label for="field0"><input type="text" id="field0" name="field0" value="*"></form></br>';
     m.innerHTML += '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>';
-    m.innerHTML += '<p>Criteria:</p><form id="criteria"><label for="criteria0"><input type="text" id="criteria0" name="criteria0"></form></br>';
+    m.innerHTML += '<p>Criteria:</p><form id="criteria"><label for="criteria0"><input type="text" id="criteria0" name="criteria0">' + OperatorOptions() + '<label for="condition0"><input type="text" id="condition0" name="condition0"></form>';
     document.getElementById("field").addEventListener("keyup", function () {
         // this part is a little buggy
         var s = StringFromFields();
@@ -23,6 +51,12 @@ function Main(e) {
     document.getElementById("table").addEventListener("keyup", function () {
         var s = document.forms["table"]["tableName"].value;
         document.getElementById("top1").textContent = "FROM " + s;
+    });
+    document.getElementById("criteria").addEventListener("keyup", function () {
+        document.getElementById("top2").textContent = StringFromCriteria() + " ;";
+    });
+    document.getElementById("criteria").addEventListener("click", function () {
+        document.getElementById("top2").textContent = StringFromCriteria() + " ;";
     });
 }
 sel.addEventListener("click", Main);
--- select.ts
+++ select.ts
@@ -13,10 +13,38 @@ function StringFromFields(): string {
         return s ;
 }
 
+function OperatorOptions(): string {
+        const ops: string[] = ["(search)", "=", "!=", "<", ">", "<=", ">="] ;
+        let s: string = '<select id="ops">' ;
+        for (let i: number = 0 ; i < ops.length ; ++i) {
+                s += `<option value="${ops[i]}">${ops[i]}</option>` ;
+        }
+        s += "</select>" ;
+        return s ;
+}
+
+function StringFromCriteria(): string {
+        var crt = document.forms["criteria"] ;
+        var op = crt["ops"].value ;
+        var cnd = crt["condition0"].value ;
+        let s: string = crt["criteria0"].value ;
+        if (op == "(search)") {
+                s += ` LIKE '_${cnd}_' ` ;
+        } else {
+                s += ` ${op} ` ;
+                if (isNaN(cnd)) {
+                        s += `'${cnd}'` ;
+                } else {
+                        s += `${cnd}` ;
+                }
+        }
+        return s ;
+}
+
 function Main(e): void {
         m.innerHTML = '<p>Fields:</p><form id="field"><label for="field0"><input type="text" id="field0" name="field0" value="*"></form></br>' ;
         m.innerHTML += '<form id="table"><label for="tableName"><p>Table:</p><input type="text" id="tableName" name="tableName"></form></br>' ;
-        m.innerHTML += '<p>Criteria:</p><form id="criteria"><label for="criteria0"><input type="text" id="criteria0" name="criteria0"></form></br>' ;
+        m.innerHTML += '<p>Criteria:</p><form id="criteria"><label for="criteria0"><input type="text" id="criteria0" name="criteria0">' + OperatorOptions() + '<label for="condition0"><input type="text" id="condition0" name="condition0"></form>' ;
         document.getElementById("field").addEventListener("keyup", function(): void {
                 // this part is a little buggy
                 let s: string = StringFromFields() ;
@@ -25,6 +53,12 @@ function Main(e): void {
         document.getElementById("table").addEventListener("keyup", function(): void {
                 let s: string = document.forms["table"]["tableName"].value ;
                 document.getElementById("top1").textContent = `FROM ${s}` ;
+        }) ;
+        document.getElementById("criteria").addEventListener("keyup", function(): void {
+                document.getElementById("top2").textContent = StringFromCriteria() + " ;" ;
+        }) ;
+        document.getElementById("criteria").addEventListener("click", function(): void {
+                document.getElementById("top2").textContent = StringFromCriteria() + " ;" ;
         }) ;
 }
 
